a5e4e984e94e6147004e33f790e0cfee395f86c2
Dominic LaBella <dominic@domilab.com>
2020-01-13 19:34:02 -0600

    Added comments.

--- templates/main.js
+++ templates/main.js
@@ -1,5 +1,10 @@
+// Show that we've loaded the JavaScript file
 console.log("Loaded main.js");
 
+// Query the endpoint that returns a JSON ...
 d3.json("/dictionary").then(function (data) {
+
+    // ... and dump that JSON to the console for inspection
     console.log(data); 
+    
 });
